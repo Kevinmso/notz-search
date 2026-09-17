14e14d5f75ed93a4050f571dd5e11f2478e3b478
Kevin de Oliveira <kevinmsoliveira@gmail.com>
2026-09-16 21:20:02 -0300

    feat: create index and search services

---
+++ internal/usecase/note_service.go
@@ -0,0 +1,30 @@
+package usecase
+
+import "github.com/Kevinmso/notz-search/internal/domain"
+
+type NoteService struct {
+	repo     domain.NoteRepository
+	embedder domain.EmbeddingProvider
+}
+
+func NewNoteService(repo domain.NoteRepository, embedder domain.EmbeddingProvider) *NoteService {
+	return &NoteService{repo: repo, embedder: embedder}
+}
+
+// IndexNote generates an embedding for note and persists it.
+func (s *NoteService) IndexNote(note domain.Note) error {
+	vector, err := s.embedder.Embed(note)
+	if err != nil {
+		return err
+	}
+	return s.repo.Upsert(note, vector)
+}
+
+// Search embeds query and returns the most similar notes, up to limit.
+func (s *NoteService) Search(query string, limit int) ([]domain.Note, error) {
+	vector, err := s.embedder.Embed(domain.Note{Text: query})
+	if err != nil {
+		return nil, err
+	}
+	return s.repo.Search(vector, limit)
+}
